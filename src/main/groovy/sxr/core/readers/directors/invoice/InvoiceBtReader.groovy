3156a206961aa963460f384396a1f9850f56730a
adrian <adrian.kuhn@arcor.de>
2024-09-21 09:12:15 +0200

    Add BtReader for Invoice

---
+++ src/main/groovy/sxr/core/readers/directors/invoice/InvoiceBtReader.groovy
@@ -0,0 +1,16 @@
+package sxr.core.readers.directors.invoice
+
+import sxr.core.readers.directors.AbstractReader
+import sxr.core.readers.directors.AbstractReaderImpl
+
+class InvoiceBtReader extends AbstractReaderImpl<Map, Map>{
+    @Override
+    AbstractReader setSrcObject(Map srcObject) {
+        return null
+    }
+
+    @Override
+    Iterator<Map> iterator() {
+        return null
+    }
+}
